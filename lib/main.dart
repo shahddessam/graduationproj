e262819af7293d5a9a6ac2d5bdc124b68e7df529
shahddessam <142095499+shahddessam@users.noreply.github.com>
2025-05-22 12:56:13 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:testproject/loginpage.dart';
 import 'mealspage.dart';
 import 'loginpage.dart';
 
-void main() {
+void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://zkuqtmzesjblwlzgwnms.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InprdXF0bXplc2pibHdsemd3bm1zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5MDI4MjYsImV4cCI6MjA2MzQ3ODgyNn0.v71-H8tCCM69j5UbBmlzjoZTLg_jlZyBKkGVq8CYZ5k',
+  );
   runApp(MyApp());
 }
 
